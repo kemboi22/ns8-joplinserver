6b81fdbced4b5d9e7fcb30dfbd2ef49cb448c13b
Martin Otieno <martin@genius.ke>
2024-01-29 11:11:09 +0300

    revert to official image

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:16 docker.io/etechonomy/joplin-server:latest" \
+    --label="org.nethserver.images=docker.io/postgres:16 docker.io/joplin/server:latest" \
     "${container}"
 #   --label="org.nethserver.images=docker.io/postgres:16 docker.io/florider89/joplin-server:latest" \
 #   --label="org.nethserver.images=docker.io/postgres:16 docker.io/etechonomy/joplin-server:latest" \
